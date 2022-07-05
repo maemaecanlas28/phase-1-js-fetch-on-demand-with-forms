ae4b0b1762f58617e3eedeb57068eee6e1e02e00
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-05 09:31:37 -0700

    commit tree 2d5473fd3c41b0c21056c35966ed404657d98283

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
+    const inputForm = document.querySelector('form'); // selecting the elements form
+    inputForm.addEventListener('submit', (event) => { // "submit" is the associated events method for the element form
+    event.preventDefault(); // 
+    const input = document.querySelector('input#searchByID');
+
+    fetch('http://localhost:3000/movies')
+    .then(response => response.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
   
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
